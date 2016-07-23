96e4f045bba0bf6417644393b93a26b2fd4305d4
Michael Walker <c.michael.s.walker@gmail.com>
2016-07-22 21:47:00 -0700

    added pub sub

--- examples/fullpageReactExample.js
+++ examples/fullpageReactExample.js
@@ -1,6 +1,9 @@
 const React = require('react');
 
-const {Fullpage, Slide, TopNav, SideNav, SideNavButton} = require('../index');
+const {Fullpage, Slide, TopNav, TopNavButton, SideNav, SideNavButton} = require('../index');
+
+//TODO: rename topnav to header and make a real footer;
+//make hover styles work again!
 
 let fullPageOptions = {
 
@@ -8,7 +11,15 @@ let fullPageOptions = {
 
 let slideOptions = {
 
-}
+};
+
+let topNavOptions = {
+
+};
+
+let sideNavOptions = {
+
+};
 
 
 class FullpageReact extends React.Component {
@@ -19,9 +30,18 @@ class FullpageReact extends React.Component {
   render() {
     return (
       <Fullpage>
+        <TopNav {...topNavOptions}>
+          <button ref={0}>slide 0</button>
+          <button ref={1}>slide 1</button>
+        </TopNav>
         <Slide style={{background: '#61DAFB'}}></Slide>
         <Slide style={{background: '#2B2C28'}}></Slide>
         <Slide style={{background: '#EFCB68'}}></Slide>
+        <SideNav {...sideNavOptions}>
+          <button ref={0}>slide 0</button>
+          <button ref={1}>slide 1</button>
+          <button ref={2}>slide 2</button>
+        </SideNav>
       </Fullpage>
     );
   }

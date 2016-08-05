c5daf49ea5e58b50a409e689ba67db0cab7b4f21
Michael Walker <c.michael.s.walker@gmail.com>
2016-08-04 22:34:52 -0700

    switch

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: 'babel',
         query: {
           presets: ['es2015', 'react']
         }
